729c558f190f509b0da6ccb4452d864317ede5dc
Carolina Rodrigues <rodriguesbcarolina@gmail.com>
2023-11-01 11:53:31 +0000

    almost finished lab

--- src/index.js
+++ src/index.js
@@ -22,14 +22,22 @@ const cards = [
   { name: 'spiderman', img: 'spiderman.jpg' },
   { name: 'superman', img: 'superman.jpg' },
   { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+  { name: 'thor', img: 'thor.jpg' },
 ];
 
 const memoryGame = new MemoryGame(cards);
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load', event => {
+  // when the page loads
+  event.preventDefault();
+
+  // shuffle the cards
+  memoryGame.shuffleCards();
+  console.log(memoryGame.cards);
+
+  // generate HTML code for each card element
   let html = '';
-  memoryGame.cards.forEach((pic) => {
+  memoryGame.cards.forEach(pic => {
     html += `
       <div class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
@@ -37,15 +45,66 @@ window.addEventListener('load', (event) => {
       </div>
     `;
   });
+  // the html variable contains the HTML code for all the cards in the memoryGame.cards array
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
+  // Bind the click event of each element (div) to a function
+  document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+
+      // add turned to card class
+      card.classList.add('turned');
+
+      // add clicked cards to picked cards array
+      const cardName = card.getAttribute('data-card-name');
+      memoryGame.pickedCards.push(cardName);
+      console.log(memoryGame.pickedCards);
+
+      // get an array of all the turned cards
+      const turnedCards = document.querySelectorAll('.turned');
+      console.log(turnedCards);
+
+      // call checkIfPair once we pick 2 cards
+      if (memoryGame.pickedCards.length === 2) {
+        memoryGame.checkIfPair(
+          memoryGame.pickedCards[0],
+          memoryGame.pickedCards[1]
+        );
+
+        // if cards are not the same, they flip again
+        if (
+          memoryGame.checkIfPair(
+            memoryGame.pickedCards[0],
+            memoryGame.pickedCards[1]
+          ) === false
+        ) {
+          setTimeout(() => {
+            turnedCards.forEach(card => {
+              card.classList.remove('turned');
+            });
+          }, 1000);
+        } else {
+          // if the cards are the same, they stay
+          turnedCards.forEach(card => {
+            card.classList.add('turned');
+          });
+        }
+
+        // reset pickedCards array
+        memoryGame.pickedCards = [];
+
+        // ISSUE 1: the pairs guessed stay when found, but then are flipping too
+        // ISSUE 2: pairs clicked are always 2 when should be 1
+
+        // update the score board
+        memoryGame.updateScore();
+      }
     });
   });
+
+  // check if the game finishes
+  memoryGame.checkIfFinished();
 });
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,58 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
+  // every time you create a new game, the order of the cards should change
   shuffleCards() {
-    // ... write your code here
+    // return undefined if argument is not passed
+    if (this.cards !== undefined) {
+      // fisher-yates shuffle function
+      let i = this.cards.length;
+      while (--i > 0) {
+        let temp = Math.floor(Math.random() * (i + 1));
+        [this.cards[temp], this.cards[i]] = [this.cards[i], this.cards[temp]];
+      }
+    } else {
+      return undefined;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    // add 1 to pairsClicked property
+    this.pairsClicked += 1;
+
+    // if the cards are the same also add 1 to pairsGuessed
+    // return true or false depending on the result of comparing both cards
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    // game finishes when all pairs are guessed
+    // 24 cards / 2 = 12 pairs
+    if (this.pairsGuessed >= this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  updateScore() {
+    // link HTML to JS
+    const clicked = document.getElementById('pairs-clicked');
+    const guessed = document.getElementById('pairs-guessed');
+
+    // update the data in HTML
+    clicked.innerText = this.pairsClicked;
+    guessed.innerTex = this.pairsGuessed;
   }
 }
